ee55c977d09e00f8873c312073d9693625d1b024
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2024-03-10 15:54:29 +0200

    Add files via upload

---
+++ db.py
@@ -0,0 +1,13 @@
+from firebase import Firebase
+import time
+
+config = {
+  'apiKey': "API KEY",
+  'authDomain': "chess-game-db-e9872.firebaseapp.com",
+  'databaseURL': "https://chess-game-db-e9872-default-rtdb.europe-west1.firebasedatabase.app",
+  'projectId': "chess-game-db-e9872",
+  'storageBucket': "chess-game-db-e9872.appspot.com",
+  'messagingSenderId': "774645442071",
+  'appId': "1:774645442071:web:7e95ccfdf6fa011e181e52",
+  'measurementId': "G-Y24RK0NW43"
+}
